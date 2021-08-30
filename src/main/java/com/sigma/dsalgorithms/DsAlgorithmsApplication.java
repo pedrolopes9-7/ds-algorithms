c59e748e64ad686cc15d38203d54c506ad3d344a
plopes <plopes@squaretrade.com>
2021-08-30 19:09:24 -0300

    first commit

---
+++ src/main/java/com/sigma/dsalgorithms/DsAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.sigma.dsalgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsAlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsAlgorithmsApplication.class, args);
+	}
+
+}
