be17d281ca2a03342681fc7cb752cdee91249fb1
plopes <plopes@squaretrade.com>
2021-08-31 09:18:49 -0300

    all over again

---
+++ src/test/groovy/com/sigma/dsalgo/DsalgoApplicationTest.groovy
@@ -0,0 +1,9 @@
+package com.sigma.dsalgo
+
+import spock.lang.Specification
+
+class DsalgoApplicationTest extends Specification {
+    def "Main"() {
+        assert 1 == 1
+    }
+}
---
+++ src/test/java/com/sigma/dsalgo/DsalgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigma.dsalgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsalgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
